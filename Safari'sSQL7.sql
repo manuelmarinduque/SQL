1e6a1270ad4fd3f0cedd586198348d798dc25a85
Manuel Marín Duque <Manuel.Duque5.2@gmail.com>
2017-05-21 22:14:06 -0500

    SQL del proyecto Safari's Hoteles

--- Safari'sSQL7.sql
+++ Safari'sSQL7.sql
@@ -8,6 +8,9 @@ A tener en cuenta:
 	-Cómo llenar atributos derivados: Tablas Factura, Actividades.
 	-Función para extraer el día de una fecha dada.
 	-Para evitar el error [54001] ERROR: límite de profundidad de stack alcanzado se recomiendo especificar el campo afectado.
+	-No olvidar que el retorno de las funciones puede ser o NEW o OLD (este para después de haber eliminado alguna tupla).
+	- <>  = 'Diferente de, no igual a'. Funciona con strings.
+	-Un trigger BEFORE y otro trigger AFTER pueden hacer referencia a la misma tabla. 
 */
 
 --CREACIÓN DE LAS TABLAS
@@ -29,17 +32,6 @@ CREATE TABLE Cliente (
   telefonoCliente VARCHAR(15) NULL,
   direccionCliente VARCHAR(80) NULL,
   PRIMARY KEY(DNI)
-);
-
-CREATE TABLE Usuarios (
-  usuario VARCHAR(30) NOT NULL,
-  codigoHotel VARCHAR(8) NOT NULL,
-  claveAcceso CHAR(8) NOT NULL,
-  tipoCuenta VARCHAR(40) NOT NULL,
-  PRIMARY KEY(usuario),
-  FOREIGN KEY(codigoHotel)
-  REFERENCES Hotel(codigoHotel)
-  ON DELETE CASCADE
 );
 
 CREATE TABLE Habitacion (
@@ -70,6 +62,37 @@ CREATE TABLE Empleado (
   ON DELETE CASCADE
 );
 
+--El nombre de usuario de cada usuario es su respectivo codigo de empleado
+CREATE TABLE Usuarios (
+  usuario INT NOT NULL,
+  codigoHotel VARCHAR(8) NOT NULL,
+  claveAcceso CHAR(8) NOT NULL,
+  tipoCuenta VARCHAR(40) NOT NULL CHECK (tipoCuenta IN ('Usuario','Administrador')),
+  PRIMARY KEY(usuario),
+  FOREIGN KEY(codigoHotel)
+  REFERENCES Hotel(codigoHotel)
+  ON DELETE CASCADE,
+  FOREIGN KEY(usuario)
+  REFERENCES Empleado(codigoEmpleado)
+  ON DELETE CASCADE
+);
+
+/*Respaldo de empleados. Esta tabla es con el fin de conservar los datos de los empleados que han salido de la empresa. Tener en cuenta que 
+esta tabla tiene su propia PK, la cual es autoincrementada (esto mediante la función SERIAL) a medida que se le ingresen tuplas. Además, 
+no tiene relación con otras tablas, por lo cual no tiene llaves foráneas.*/
+CREATE TABLE Respaldo_Empleado (
+  codigoRespaldo SERIAL,
+  codigoEmpleado INTEGER NOT NULL,
+  DNI VARCHAR(15) NOT NULL UNIQUE ,
+  codigoHotel VARCHAR(8) NOT NULL,
+  nombreCompleto VARCHAR(35) NOT NULL,
+  direccionEmpleado VARCHAR(80) NULL,
+  telefonoEmpleado VARCHAR(15) NULL,
+  tituloEstudio VARCHAR(35) NOT NULL,
+  ocupacion VARCHAR(35) NOT NULL,
+  PRIMARY KEY(codigoRespaldo)
+);
+
 CREATE TABLE Servicios (
   codigoServicio VARCHAR(8) NOT NULL,
   codigoEmpleado INTEGER NOT NULL,
@@ -97,7 +120,7 @@ CREATE TABLE Reserva_Estancia (
   fechaReserva DATE NULL /*También puede tener DEFAULT NOW()*/,
   fechaLimite DATE NOT NULL CHECK (fechaLimite>=fechaReserva),
   deposito FLOAT NOT NULL DEFAULT 0 CHECK (deposito>=0),
-  fechaIngreso DATE NULL CHECK (fechaIngreso<=fechaLimite),
+  fechaIngreso DATE NULL CHECK (fechaIngreso<=fechaLimite), --fechaIngreso>=fechaReserva
   --Por razones lógicas fechaSalida>=fechaIngreso, pero por cuestiones de demostración en la sustentación se omite
   fechaSalida DATE NULL /*CHECK (fechaSalida>=fechaIngreso)*/,
   PRIMARY KEY(codigoReserva),
@@ -134,7 +157,7 @@ CREATE TABLE Factura (
   precioTemporada FLOAT NOT NULL DEFAULT 0 CHECK (precioTemporada>=0),
   diasEstancia INTEGER NOT NULL DEFAULT 0 CHECK (diasEstancia>=0),
   precioTotalHabitacion FLOAT NOT NULL DEFAULT 0 CHECK (precioTotalHabitacion>=0),
-  precioTotalActividades FLOAT NOT NULL DEFAULT 0 CHECK (precioTotalActividades>=0),
+  precioTotalActividades FLOAT DEFAULT 0 CHECK (precioTotalActividades>=0),
   precioTotalEstancia FLOAT NOT NULL DEFAULT 0 CHECK (precioTotalEstancia>=0),
   PRIMARY KEY(nroFactura),
   FOREIGN KEY(DNI)
@@ -273,6 +296,49 @@ CREATE TRIGGER Precio_Habitacion AFTER INSERT ON Habitacion FOR EACH ROW EXECUTE
 el error [54001] ERROR: límite de profundidad de stack alcanzado se recomienda especificar el campo afectado, en este caso: AFTER UPDATE OF tipoHabitacion ON Habitacion*/
 CREATE TRIGGER Precio_Habitacion2 AFTER UPDATE OF tipoHabitacion ON Habitacion FOR EACH ROW EXECUTE PROCEDURE Precio_Habitacion();
 
+/*Función que determina que un cliente sólo pueda hacer reservas de habitaciones en el mismo límite de tiempo y en el mismo hotel.*/
+CREATE OR REPLACE FUNCTION Insertar() RETURNS TRIGGER AS $$
+DECLARE 
+
+/*Variables donde se almacenan el hotel donde el cliente hizo la reserva, su DNI, y la fecha límite. Cabe destacar que estos valores se obtiene de la primer tupla insertada, es decir, 
+la primer resrrva hecha. Si el cliente no tiene ninguna reserva, esto es, cuando los valores de las siguientes tres variables IS NULL, la inserción de la tupla se realiza y a partir de 
+esta ya se obtienen los respectivos valores.*/
+hotel VARCHAR(8);
+cliente VARCHAR(8);
+fecha_limite DATE;
+
+BEGIN
+
+SELECT codigoHotel INTO hotel
+FROM Reserva_Estancia
+WHERE DNI=NEW.DNI
+GROUP BY codigoHotel;
+
+SELECT DNI INTO cliente
+FROM Reserva_Estancia
+WHERE DNI=NEW.DNI
+GROUP BY DNI;
+
+SELECT fechaLimite INTO fecha_limite
+FROM Reserva_Estancia
+WHERE DNI=NEW.DNI
+GROUP BY fechaLimite;
+
+IF NEW.DNI=cliente AND NEW.codigoHotel<>hotel
+THEN
+ RAISE EXCEPTION 'No se puede realizar la reserva. Hotel diferente.';
+ELSIF NEW.DNI=cliente AND NEW.fechaLimite<>fecha_limite
+ THEN
+  RAISE EXCEPTION 'No se puede realizar la reserva. Fecha límite diferente.';
+END IF;
+
+ RETURN NEW;
+ 
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER Insertar BEFORE INSERT ON Reserva_Estancia FOR EACH ROW EXECUTE PROCEDURE Insertar();
+
 /*Función que determina que el depósito de una reserva sea el 10% del costo de la habitación, que la fecha de la reserva sea la actual y que cambie el estado de una 
 habitación asignda a 'Reservada' sólo si está disponible. */
 CREATE OR REPLACE FUNCTION Insercion_Reserva_Estancia() RETURNS TRIGGER AS $$
@@ -283,7 +349,7 @@ DECLARE
  estado_habitacion VARCHAR(20);
  
 BEGIN
-
+ 
  SELECT precioHabitacion INTO precio_habitacion
  FROM Habitacion
  WHERE nroHabitacion=NEW.nroHabitacion AND codigoHotel=NEW.codigoHotel;
@@ -347,11 +413,11 @@ reservas y se hace su respaldo en Respaldo_Reserva_Estancia. */
  WHERE nroHabitacion=OLD.nroHabitacion AND codigoHotel=OLD.codigoHotel;
  
 --Tener en cuenta la PK en la tabla respaldo; la PK no se tiene en cuenta en la siguiente inserción ya que tal PK es autoincrementada.
-  INSERT INTO Respaldo_Reserva_Estancia (codigoReserva, codigoHotel, nroHabitacion, DNI, fechaReserva, fechaLimite, deposito, fechaIngreso, fechaSalida)
-  VALUES (NEW.codigoReserva, NEW.codigoHotel, NEW.nroHabitacion, NEW.DNI, NEW.fechaReserva, NEW.fechaLimite, NEW.deposito, NEW.fechaIngreso, NEW.fechaSalida);
+ INSERT INTO Respaldo_Reserva_Estancia (codigoReserva, codigoHotel, nroHabitacion, DNI, fechaReserva, fechaLimite, deposito, fechaIngreso, fechaSalida)
+ VALUES (NEW.codigoReserva, NEW.codigoHotel, NEW.nroHabitacion, NEW.DNI, NEW.fechaReserva, NEW.fechaLimite, NEW.deposito, NEW.fechaIngreso, NEW.fechaSalida);
 
 --Por último cada reserva del cliente se borra de la tabla Reserva_Estancia.
-  DELETE FROM Reserva_Estancia WHERE DNI=NEW.DNI;
+ DELETE FROM Reserva_Estancia WHERE DNI=NEW.DNI;
  END IF;
 
  RETURN NEW;
@@ -362,7 +428,7 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER Actualizacion_Reserva AFTER UPDATE ON Reserva_Estancia FOR EACH ROW EXECUTE PROCEDURE Actualizacion_Reserva();
 
 /*Función para que cada vez que una tupla se elimine se active el trigger Eliminacion_Reserva_Estancia, el cual cambia el estado de la habitación ocupada a 'Disponible'.
-Esta es función es útil para cuando el cliente reserva 3 habitaciones y sólo ocupa 2; las habitaciones que no ocupa debe de cambiar su estado a 'Disponible' y además estas
+Esta es función es útil para cuando el cliente reserva 3 habitaciones y sólo ocupa 2; las habitaciones que no ocupa deben cambiar su estado a 'Disponible' y además estas
 no tendrán respaldo.*/
 CREATE OR REPLACE FUNCTION Eliminacion_Reserva_Estancia() RETURNS TRIGGER AS $$
 DECLARE 
@@ -377,7 +443,6 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER Eliminacion_Reserva_Estancia AFTER DELETE ON Reserva_Estancia FOR EACH ROW EXECUTE PROCEDURE Eliminacion_Reserva_Estancia();
-
 
 --Función que determina que un empleado sea del respectivo hotel de un servicio y que además este sea animador:
 CREATE OR REPLACE FUNCTION Insercion_Servicios() RETURNS TRIGGER AS $$
@@ -416,7 +481,7 @@ $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER Insercion_Servicios AFTER INSERT ON Servicios FOR EACH ROW EXECUTE PROCEDURE Insercion_Servicios();
 /*Trigger para cuando se actualiza el codigo de empleado; la actualización por tanto debe cumplir la condición de la función Insercion_Servicios() */
-CREATE TRIGGER Insercion_Servicios2 AFTER UPDATE OF codigoEmpleado ON Servicios FOR EACH ROW EXECUTE PROCEDURE Insercion_Servicios();
+CREATE TRIGGER Insercion_Servicios2 AFTER UPDATE OF codigoEmpleado, codigoHotel ON Servicios FOR EACH ROW EXECUTE PROCEDURE Insercion_Servicios();
 
 /*Función que determina los servicios o actividades prestadas a un cliente. Se debe tener en cuenta que una actividad puede ser asignada sólo si el cliente está hospedado
 en el hotel, es decir, si ha cumplido con la reserva y tiene una fecha de ingreso. Además el precio a pagar por un actividad depende de la cantidad de acompañantes del 
@@ -454,8 +519,8 @@ BEGIN
 
 --Determinación del hotel donde se presta el servicio
  SELECT codigoHotel INTO hotel_servicio
- FROM Servicios AS S INNER JOIN Actividades AS A ON S.codigoServicio=A.codigoServicio
- WHERE A.codigoServicio=NEW.codigoServicio
+ FROM Servicios 
+ WHERE codigoServicio=NEW.codigoServicio
  GROUP BY codigoHotel;
 
 --Determinación del precio por persona del servicio.
@@ -485,6 +550,36 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER Insercion_Actividades AFTER INSERT ON Actividades FOR EACH ROW EXECUTE PROCEDURE Insercion_Actividades();
+
+/*Función para cuando una tupla es actualizada en Actividades. El único campo que debe ser actualizado tiene que ser cantidadPersonas. Si este campo es cambiado, se 
+debe modificar valorAPagar*/
+CREATE OR REPLACE FUNCTION Actualizacion_Actividades() RETURNS TRIGGER AS $$
+DECLARE 
+
+precio_persona FLOAT;
+cantidad_personas INT;
+
+BEGIN
+
+--Determinación del precio por persona del servicio.
+ SELECT precioPersona INTO precio_persona
+ FROM Servicios
+ WHERE codigoServicio=NEW.codigoServicio;
+ 
+--Determinación de los acompañantes del cliente al servicio.
+ SELECT cantidadPersonas INTO cantidad_Personas
+ FROM Actividades
+ WHERE codigoServicio=NEW.codigoServicio AND DNI=NEW.DNI;
+
+ UPDATE Actividades
+ SET valorAPagar=precio_persona*cantidad_personas
+ WHERE codigoServicio=NEW.codigoServicio AND DNI=NEW.DNI;
+
+ RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER Actualizacion_Actividades AFTER UPDATE OF cantidadPersonas ON Actividades FOR EACH ROW EXECUTE PROCEDURE Actualizacion_Actividades();
 
 /*Función que determina el precio de la temporada, el precio de las habitaciones, el precio de los servicios consumidos y el depósito del cliente pagado 
 por las reservas de las habitaciones. Además, cuando se genera la factura, las reservas del cliente y sus actividades deben eliminarse, aunque las reservas
@@ -571,14 +666,21 @@ UPDATE Factura
 SET precioTotalHabitacion=precio_habitaciones
 WHERE nroFactura=NEW.nroFactura;
 
---Asiganación del valor al atributo precioTotalActividades
-UPDATE Factura
-SET precioTotalActividades=precio_actividades
-WHERE nroFactura=NEW.nroFactura;
+--Asignación del valor al atributo precioTotalActividades
+IF precio_actividades IS NULL
+THEN 
+ UPDATE Factura
+ SET precioTotalActividades=0
+ WHERE nroFactura=NEW.nroFactura;
+ELSE
+ UPDATE Factura
+ SET precioTotalActividades=precio_actividades
+ WHERE nroFactura=NEW.nroFactura;
+END IF;
 
 --Determinación del valor total a pagar
 UPDATE Factura
-SET precioTotalEstancia=precio_habitaciones+precio_actividades-valor_deposito+precioTemporada
+SET precioTotalEstancia=(diasEstancia*precio_habitaciones)+precioTotalActividades-valor_deposito+precioTemporada
 WHERE nroFactura=NEW.nroFactura;
 
 --La fecha de salida de un cliente del hotel es aquella fecha de realización de la factura, es decir, la fecha actual.
@@ -594,15 +696,95 @@ $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER Insercion_Factura AFTER INSERT ON Factura FOR EACH ROW EXECUTE PROCEDURE Insercion_Factura();
 
+/*Función para cuando un empleado es eliminado; en tal caso se le hace un respaldo. Esta función funciona de forma similar a Respaldo_R_E en el sentido de la PK*/
+CREATE OR REPLACE FUNCTION Eliminar_Empleado() RETURNS TRIGGER AS $$
+DECLARE 
+BEGIN
+
+ INSERT INTO Respaldo_Empleado (codigoEmpleado, DNI, codigoHotel, nombreCompleto, direccionEmpleado, telefonoEmpleado, tituloEstudio, ocupacion)
+ VALUES(OLD.codigoEmpleado, OLD.DNI, OLD.codigoHotel, OLD.nombreCompleto, OLD.direccionEmpleado, OLD.telefonoEmpleado, OLD.tituloEstudio, OLD.ocupacion);
+
+ RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER Eliminar_Empleado BEFORE DELETE ON Empleado FOR EACH ROW EXECUTE PROCEDURE Eliminar_Empleado();
+
+/*Función que determina que un usuario de la base de datos sea un empleado del hotel y que además sea del área de sistemas*/
+CREATE OR REPLACE FUNCTION Insertar_Usuario() RETURNS TRIGGER AS $$
+DECLARE 
+
+--Variables donde se almacena el hotel donde trabaja el empleado y su ocupación:
+hotel_empleado VARCHAR(8);
+ocupacion_empleado VARCHAR(35);
+
+BEGIN
+
+--Determinación del código del empleado:
+ SELECT codigoHotel INTO hotel_empleado
+ FROM Empleado
+ WHERE codigoEmpleado=NEW.usuario;
+
+--Determinación de la ocupación del empleado:
+ SELECT ocupacion INTO ocupacion_empleado
+ FROM Empleado
+ WHERE codigoEmpleado=NEW.usuario;
+
+ IF ocupacion_empleado<>'Sistemas'
+ THEN 
+  RAISE EXCEPTION 'El empleado no pertece al área de sistemas.';--;
+ ELSIF hotel_empleado<>NEW.codigoHotel
+  THEN
+   RAISE EXCEPTION 'El empleado no pertenece al hotel.';
+ END IF;
+ 
+ RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER Insertar_Usuario AFTER INSERT ON Usuarios FOR EACH ROW EXECUTE PROCEDURE Insertar_Usuario();
 
 
-CREATE OR REPLACE FUNCTION Insercion_Actividades() RETURNS TRIGGER AS $$
+
+
+
+
+
+
+
+
+--Función que determina que ningún campo de la tabla Factura sea modificado.
+CREATE OR REPLACE FUNCTION Sin_Modificar() RETURNS TRIGGER AS $$
+DECLARE 
+BEGIN
+
+RAISE EXCEPTION 'No se puede hacer modificaciones en la factura.';
+
+ RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER Sin_Modificar_Factura BEFORE UPDATE ON Factura FOR EACH ROW EXECUTE PROCEDURE Sin_Modificar();
+
+DROP TRIGGER Sin_Modificar_Factura ON Factura
+
+
+
+
+
+
+
+
+
+CREATE OR REPLACE FUNCTION () RETURNS TRIGGER AS $$
 DECLARE 
 BEGIN
 
  RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER [] ON Empleado FOR EACH ROW EXECUTE PROCEDURE ();
 
 
 
